4ff986d4a7633716eb6f8dba5e0e17137d945dc8
Rafael <Rafaelgvargas@live.com>
2024-11-12 20:29:39 -0300

    configuração do typescript

--- soma.test.ts
+++ soma.test.ts
@@ -1,4 +1,4 @@
-const soma = require('./soma')
+import soma from './soma'
 
 describe('teste de soma', () => {
     test('deve verificar o resultado de uma soma', () => {
---
+++ soma.ts
@@ -0,0 +1,7 @@
+export default function soma(numero1: number, numero2: number): number {
+    return numero1 + numero2
+}
+
+
+
+
