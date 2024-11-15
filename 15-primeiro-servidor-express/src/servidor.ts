3963effa85d9eec4e551ecbcd60988556f3e326c
Rafael <Rafaelgvargas@live.com>
2024-11-14 22:23:24 -0300

    testando servidor express com typescript

---
+++ 15-primeiro-servidor-express/src/servidor.ts
@@ -0,0 +1,12 @@
+import express, { RequestHandler, Request, Response, NextFunction } from 'express'
+
+const servidor = express()
+
+const controlador: RequestHandler = (req: Request, res: Response, next: NextFunction) => {
+    res.send('Meu primeiro servidor Node.js com express e TypeScript')
+    return Promise.resolve();
+}
+
+servidor.get('/home', controlador)
+
+servidor.listen(3000)
