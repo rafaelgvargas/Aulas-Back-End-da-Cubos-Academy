eaeab05685ee2ac1d0ce7ccf75024ff1e992cbcf
Rafael <Rafaelgvargas@live.com>
2024-11-14 21:31:52 -0300

    Finalizado a aula de servidor com fastify

---
+++ 14-primeiro-servidor-fastify/src/servidor.ts
@@ -0,0 +1,14 @@
+import fastify from "fastify"
+
+const servidor = fastify({
+    logger: true
+})
+
+servidor.get('/teste', function controlador(requisicao, resposta) {
+    const saudacao = 'Bem vindo ao meu servidor Node.js com TypeScript'
+    return resposta.send(saudacao)
+})
+
+servidor.listen({
+    port:3000
+})
