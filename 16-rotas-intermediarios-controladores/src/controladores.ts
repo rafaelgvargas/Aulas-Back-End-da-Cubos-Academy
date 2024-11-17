93d2cc037310f7a8fc49851bdf8213e87db0aa91
Rafael <Rafaelgvargas@live.com>
2024-11-16 23:05:00 -0300

    organização dos controladores

---
+++ 16-rotas-intermediarios-controladores/src/controladores.ts
@@ -0,0 +1,34 @@
+import { Request, Response } from "express"
+
+const pessoas = [
+    { nome: 'Rafael', email: 'Rafael@email.com' },
+    { nome: 'Leticia', email: 'Leticia@email.com' },
+    { nome: 'Guilherme', email: 'Guilherme@email.com' }
+]
+
+export const itemProdutos = (req: Request, res: Response) => {
+    console.log(req.params.item);    
+    res.send("O nodemon está configurado com typescript");
+}
+
+export const buscarUsuario = (req: Request, res: Response) => {
+    const { email } = req.params
+    const pessoa = pessoas.find(pessoa => pessoa.email === email);
+    if (!pessoa) {
+      res.send('Usuário não encontrado!');
+    } else {
+      res.send(pessoa);
+    }
+  }
+
+export const buscarUsuarioQuery = (req: Request, res: Response) => {
+    const { email } = req.query
+    if (!email) {
+        res.send('Parametro não encontrado!');
+    }
+    const pessoa = pessoas.find(pessoa => pessoa.email === email);
+    if (!pessoa) {
+        res.send('Usuário não encontrado!');
+    }
+    res.send(pessoa)
+}
--- 16-rotas-intermediarios-controladores/src/index.ts
+++ 16-rotas-intermediarios-controladores/src/index.ts
@@ -1,34 +1,11 @@
-import { Query } from './../node_modules/@types/express-serve-static-core/index.d';
-import express from "express"
+import { itemProdutos, buscarUsuario, buscarUsuarioQuery } from './controladores';
+import express , { Request, Response } from "express"
 import 'dotenv/config'
 
 const servidor = express()
 
-servidor.get("/produtos/:item", (req: any, res: any) => {
-    console.log(req.params.item);    
-    res.send("O nodemon está configurado com typescript");
-})
-
-const pessoas = [
-    { nome: 'Rafael', email: 'Rafael@email.com' },
-    { nome: 'Leticia', email: 'Leticia@email.com' },
-    { nome: 'Guilherme', email: 'Guilherme@email.com' }
-]
-
-servidor.get('/usuarios/:email', (req: any, res: any) => {
-    const { email } = req.Query
-    if (!email) {
-        return res.send('parâmetro email não informado')
-    }
-    const pessoa = pessoas.find((item) => {
-        return item.email === email
-    })
-
-    if (!pessoa) {
-        return res.send('Usuário não encontrado')
-    }
-
-    res.send('OK!')
-})
+servidor.get('/produtos/item', itemProdutos)
+servidor.get('/usuarios/:email', buscarUsuario)
+servidor.get('/usuarios', buscarUsuarioQuery)
 
 servidor.listen(process.env.PORT)
