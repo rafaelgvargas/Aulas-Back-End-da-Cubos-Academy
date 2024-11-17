2453975d47a10cffcca10c1332e201096d76bc8a
Rafael <Rafaelgvargas@live.com>
2024-11-16 22:09:16 -0300

    utilizando parametros de rotas no express

--- 16-rotas-intermediarios-controladores/src/index.ts
+++ 16-rotas-intermediarios-controladores/src/index.ts
@@ -1,14 +1,31 @@
-import express , { Request, Response } from "express"
+import express from "express"
 import 'dotenv/config'
 
 const servidor = express()
 
-servidor.get("/", (req, res) => {    
-    res.send("Servidor está ok!");
+servidor.get("/produtos/:item", (req: any, res: any) => {
+    console.log(req.params.item);    
+    res.send("O nodemon está configurado com typescript");
 })
 
-servidor.get("/usuarios", (req: Request, res: Response) => { 
-    res.send("Listagem de usuários");
+const pessoas = [
+    { nome: 'Rafael', email: 'Rafael@email.com' },
+    { nome: 'Leticia', email: 'Leticia@email.com' },
+    { nome: 'Guilherme', email: 'Guilherme@email.com' }
+]
+
+servidor.get('/usuarios/:email', (req: any, res: any) => {
+    const { email } = req.params 
+
+    const pessoa = pessoas.find((item) => {
+        return item.email === email
+    })
+
+    if (!pessoa) {
+        return res.send('Usuário não encontrado')
+    }
+
+    res.send(pessoa)
 })
 
 servidor.listen(process.env.PORT)
