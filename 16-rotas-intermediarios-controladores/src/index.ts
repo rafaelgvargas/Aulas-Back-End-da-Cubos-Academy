7f52bbfbd9e79aea8d4df6c45e7aa393be54e3e2
Rafael <Rafaelgvargas@live.com>
2024-11-16 21:26:43 -0300

    utilizando postman e iniciando servidor automaticamente com nodemon

--- 16-rotas-intermediarios-controladores/src/index.ts
+++ 16-rotas-intermediarios-controladores/src/index.ts
@@ -3,8 +3,12 @@ import 'dotenv/config'
 
 const servidor = express()
 
-servidor.get("/", (req, res) => {
+servidor.get("/", (req, res) => {    
     res.send("Servidor está ok!");
+})
+
+servidor.get("/usuarios", (req: Request, res: Response) => { 
+    res.send("Listagem de usuários");
 })
 
 servidor.listen(process.env.PORT)
